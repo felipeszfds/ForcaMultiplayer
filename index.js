dd5129c31160657d5cf96fdbf07a5f1f8dee51da
Felipe Souza Fernandes <164778330+felipeszfds@users.noreply.github.com>
2024-06-12 01:06:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -20,9 +20,11 @@ let palavraSecreta = "teste";
 let letrasAdivinhadas = {};
 let jogadores = [];
 let erros = {};
-const limiteErros = 5; // Defina o limite de erros aqui
+const limiteErros = 6; // Defina o limite de erros aqui
+let palavraAtualizada = palavraSecreta.split('').map(letra => '_').join(' ');
 
 io.on('connection', (socket) => {
+  socket.emit('mensagem', {palavraAtualizada: palavraSecreta.split('').map(letra => '_').join(' '), mensagem: 'Bem-vindo ao jogo da forca!'});
   erros[socket.id] = 0;
   letrasAdivinhadas[socket.id] = [];
   console.log('Novo jogador conectado:', socket.id);
@@ -43,33 +45,51 @@ io.on('connection', (socket) => {
     }
 
     letrasAdivinhadas[socket.id].push(letra);
+    palavraAtualizada = palavraSecreta.split('').map(letra => letrasAdivinhadas[socket.id].includes(letra) ? letra : '_').join(' ');
 
     if (!palavraSecreta.includes(letra)) {
         erros[socket.id]++;
 
+        if (Object.values(erros).every(numErros => numErros >= limiteErros)) {
+          socket.on('resetar', () => {
+            io.emit('mensagem', { mensagem: 'Todos perderam!'});
+            resetarJogo();
+            return;
+          });
+        } 
+
         if (erros[socket.id] >= limiteErros) {
-            socket.emit('mensagem', { mensagem: 'Jogo acabou para você! O limite de erros foi atingido.' });
+            socket.emit('mensagem', { mensagem: 'Jogo acabou para você! O limite de erros foi atingido.', erros: erros[socket.id] });
           } else {
-            socket.emit('mensagem', { mensagem: `Erro! Você tem ${limiteErros - erros[socket.id]} tentativas restantes.` });
+            socket.emit('mensagem', { mensagem: `Erro! Você tem ${limiteErros - erros[socket.id]} tentativas restantes.`, letrasAdivinhadas: letrasAdivinhadas[socket.id], erros: erros[socket.id], palavraAtualizada: palavraAtualizada});
           }
+          
     } else {
         const letrasRestantes = palavraSecreta.split('').filter(l => !letrasAdivinhadas[socket.id].includes(l));
+        
         if (letrasRestantes.length === 0) {
-          io.emit('vencedor', { vencedor: socket.id, palavraSecreta });
-          resetarJogo();
+          io.emit('vencedor', { vencedor: socket.id, palavraSecreta, palavraAtualizada});
+          socket.on('resetar', () => {
+            resetarJogo();
+          });
         } else {
-          socket.emit('mensagem', { mensagem: 'Boa! Letra correta!', letrasAdivinhadas: letrasAdivinhadas[socket.id] });
+          socket.emit('mensagem', { mensagem: 'Boa! Letra correta!', letrasAdivinhadas: letrasAdivinhadas[socket.id], palavraAtualizada: palavraAtualizada});
         }
     }
   });
+
 });
 
 function resetarJogo() {
-    let indiceAleatorio = Math.floor(Math.random() * palavras.length);
-    palavraSecreta = palavras[indiceAleatorio];
+  let indiceAleatorio = Math.floor(Math.random() * palavras.length);
+  palavraSecreta = palavras[indiceAleatorio];
   for (let id in letrasAdivinhadas) {
     letrasAdivinhadas[id] = []; // Limpe as letras adivinhadas para todos os jogadores
   }
+  for(let id in erros){
+    erros[id] = 0;
+  }
+  io.emit('resetar', { palavraAtualizada: palavraSecreta.split('').map(letra => '_').join(' '), mensagem: 'Jogo resetado!', erros});
 }
 
 server.listen(port, () => {
--- script.js
+++ script.js
@@ -2,16 +2,43 @@ const socket = io();
 
 const mensagens = document.getElementById('mensagens');
 const chuteInput = document.getElementById('chute');
+const palavraAtualizadaDiv = document.getElementById('palavra-secreta');
+const quadrados = document.querySelectorAll('.quadrado');
+
+var contador = 0;
 
 socket.on('mensagem', (data) => {
+  console.log(data);
   mensagens.innerHTML += `<p>${data.mensagem}</p>`;
   if (data.letrasAdivinhadas) {
     mensagens.innerHTML += `<p>Letras adivinhadas: ${data.letrasAdivinhadas.join(', ')}</p>`;
+  }
+  if (data.palavraAtualizada) {
+    atualizarpalavraAtualizada(data.palavraAtualizada);
+  }
+  if (data.erros) {
+    contador = data.erros;
+    console.log(contador)
+    if (contador >= 6) {
+      desabilitarChute();
+    }
+    atualizaErro();
   }
 });
 
 socket.on('vencedor', (data) => {
   mensagens.innerHTML += `<p>O jogador ${data.vencedor} venceu! A palavra era: ${data.palavraSecreta}</p>`;
+  if (data.palavraAtualizada) {
+    atualizarpalavraAtualizada(data.palavraAtualizada);
+  }
+});
+
+socket.on('resetar', (data) => {
+  mensagens.innerHTML += `<p>${data.mensagem}</p>`;
+  contador = 0;
+  habilitarChute();
+  atualizarpalavraAtualizada(data.palavraAtualizada);
+  atualizaErro();
 });
 
 function enviarChute() {
@@ -19,3 +46,44 @@ function enviarChute() {
   socket.emit('chutar', letra);
   chuteInput.value = '';
 }
+
+function reiniciarJogo() {
+  socket.emit('resetar');
+}
+
+function atualizarpalavraAtualizada(palavraAtualizada) {
+    palavraAtualizadaDiv.innerHTML = palavraAtualizada.split('').map(letra => letra === ' ' ? '&nbsp;' : letra).join(' ');
+}
+
+function atualizaErro(){
+    for (let i = 0; i < contador; i++) {
+        quadrados[i].style.backgroundColor = "red";
+    }
+    if (contador === 0) {
+        for (let i = 0; i < quadrados.length; i++) {
+            quadrados[i].style.backgroundColor = "grey";
+        }
+    }
+}
+
+function habilitarChute(){
+  document.getElementById('chute').disabled = false;
+  document.getElementById('enviar').disabled = false;
+  document.getElementById('enviar').style.backgroundColor = "#333";
+  document.getElementById('enviar').cursor = "pointer";
+}
+
+function desabilitarChute(){
+  document.getElementById('chute').disabled = true;
+  document.getElementById('enviar').disabled = true;
+  document.getElementById('enviar').style.backgroundColor = "grey";
+  document.getElementById('enviar').cursor = "not-allowed";
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter' || event.keyCode === 13) {
+    enviarChute();
+  }
+});
+
+
